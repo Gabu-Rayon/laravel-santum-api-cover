e5ef40cccaab41475c0273ad35e6ee882d988d64
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-04-13 12:34:14 +0300

    new commits

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +14,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/products',[ProductController::class,'index']);
+Route::get('/products', function () {
+    return Product::all();
+});
+Route::post('/products', function () {
+    return Product::create(
+[
+        'name' =>'Product Five',
+         'slug' =>'product-five',
+          'description' =>'This is Product Five',
+           'price' =>'15.99'
+    ]
+);
+});
+// Route::get('/products',[ProductController::class,'index']);
 
-Route::get('/products',[ProductController::class,'store']);
+// Route::get('/products',[ProductController::class,'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
